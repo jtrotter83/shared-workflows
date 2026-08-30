3dfcaf8b50551512b1bac5c3345de6a6971aee01
jtrotter83 <jtrotter83@users.noreply.github.com>
2026-08-30 21:10:12 +0000

    test: all three jobs, formatted

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,3 @@
 import eslint from "@eslint/js";
 
-export default [
-  { ignores: ["**/*"] },
-  eslint.configs.recommended,
-];
+export default [{ ignores: ["**/*"] }, eslint.configs.recommended];
